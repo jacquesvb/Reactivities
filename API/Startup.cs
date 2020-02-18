e7f5c7eb7b34edf860c26976b355044fee3a5174
Jacques Van Blokland <jacquesvb@live.com>
2020-02-18 07:21:15 -0800

    Getting data from the API

--- API/Startup.cs
+++ API/Startup.cs
@@ -30,8 +30,14 @@ namespace API
             services.AddDbContext<DataContext>(opt =>
             {
                 opt.UseSqlite(Configuration.GetConnectionString("DefaultConnection"));
-            }
-            );
+            });
+            services.AddCors(opt =>
+            {
+                opt.AddPolicy("CorsPolicy", policy =>
+                {
+                    policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000");
+                });
+            });
             services.AddControllers();
         }
 
@@ -44,10 +50,9 @@ namespace API
             }
 
             // app.UseHttpsRedirection();
+            app.UseCors("CorsPolicy");
 
             app.UseRouting();
-
-            app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
